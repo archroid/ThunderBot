3ac7605e963aff35b61e9e2efe73d3223a0e08ef
archroid <archroid.dev@gmail.com>
2021-12-11 18:04:56 +0330

    ⭐Trying to complete polls

--- poll/poll.go
+++ poll/poll.go
@@ -1,8 +1,6 @@
 package poll
 
 import (
-	"log"
-
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -27,27 +25,45 @@ var numberToEmoji = map[int]string{
 
 func CreatePoll(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
-	// var poll Poll
+	var poll Poll
 
-	// topic := i.ApplicationCommandData().Options[0].StringValue()
-	// duration := i.ApplicationCommandData().Options[1].IntValue()
+	topic := i.ApplicationCommandData().Options[0].StringValue()
+	duration := i.ApplicationCommandData().Options[1].IntValue()
 
 	optionsNum := len(i.ApplicationCommandData().Options) - 2
 
 	option1 := i.ApplicationCommandData().Options[2].StringValue()
 	option2 := i.ApplicationCommandData().Options[3].StringValue()
-	option3 := i.ApplicationCommandData().Options[4].StringValue()
-	
-	option4 := i.ApplicationCommandData().Options[5].StringValue()
-	option5 := i.ApplicationCommandData().Options[6].StringValue()
-	option6 := i.ApplicationCommandData().Options[7].StringValue()
-	option7 := i.ApplicationCommandData().Options[8].StringValue()
-	option8 := i.ApplicationCommandData().Options[9].StringValue()
-	option9 := i.ApplicationCommandData().Options[10].StringValue()
-	option10 := i.ApplicationCommandData().Options[11].StringValue()
+	optionsArray := []string{option1, option2}
 
-	optionsArray := [10]string{option1, option2, option3, option4, option5, option6, option7, option8, option9, option10}
-	optionsSlice := optionsArray[2:optionsNum]
-	log.Println(len(optionsSlice))
+	if optionsNum == 5 {
+		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[4].StringValue())
+	}
+
+	if optionsNum == 6 {
+		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[5].StringValue())
+	}
+	if optionsNum == 7 {
+		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[6].StringValue())
+	}
+	if optionsNum == 8 {
+		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[7].StringValue())
+	}
+	if optionsNum == 9 {
+		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[8].StringValue())
+	}
+	if optionsNum == 10 {
+		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[9].StringValue())
+	}
+	if optionsNum == 11 {
+		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[10].StringValue())
+	}
+	if optionsNum == 12 {
+		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[11].StringValue())
+	}
+
+	poll = Poll{topic: topic, durationInMinutes: int(duration), options: optionsArray}
+
+	println(len(poll.options))
 
 }
