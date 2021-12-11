34c1aaeb0ce4b12ce6c398f50d3cebecb52b838d
archroid <archroid.dev@gmail.com>
2021-12-11 19:20:51 +0330

    🔥Polls are about to work

--- poll/poll.go
+++ poll/poll.go
@@ -1,69 +1,74 @@
 package poll
 
 import (
+	"fmt"
+
+	embed "archroid/ElProfessorBot/utils"
+
 	"github.com/bwmarrin/discordgo"
 )
 
-type Poll struct {
-	topic             string
-	options           []string
-	durationInMinutes int
-}
-
-var numberToEmoji = map[int]string{
-	1:  "1️⃣",
-	2:  "2️⃣",
-	3:  "3️⃣",
-	4:  "4️⃣",
-	5:  "5️⃣",
-	6:  "6️⃣",
-	7:  "7️⃣",
-	8:  "8️⃣",
-	9:  "9️⃣",
-	10: "🔟",
-}
-
 func CreatePoll(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
-	var poll Poll
+	// var poll Poll
 
 	topic := i.ApplicationCommandData().Options[0].StringValue()
-	duration := i.ApplicationCommandData().Options[1].IntValue()
 
 	optionsNum := len(i.ApplicationCommandData().Options) - 2
 
 	option1 := i.ApplicationCommandData().Options[2].StringValue()
 	option2 := i.ApplicationCommandData().Options[3].StringValue()
-	optionsArray := []string{option1, option2}
 
-	if optionsNum == 5 {
-		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[4].StringValue())
-	}
+	pollContent := fmt.Sprintf("📊** %v ** \n \n 1️⃣ %v \n 2️⃣ %v", topic, option1, option2)
 
-	if optionsNum == 6 {
-		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[5].StringValue())
+	if optionsNum >= 3 {
+		pollContent = pollContent + fmt.Sprintf("\n3️⃣ %v ", i.ApplicationCommandData().Options[4].StringValue())
+		// println(i.ApplicationCommandData().Options[4].StringValue())
+		// println(pollContent)
 	}
-	if optionsNum == 7 {
-		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[6].StringValue())
+	if optionsNum >= 4 {
+		pollContent = pollContent + fmt.Sprintf("\n4️⃣ %v ", i.ApplicationCommandData().Options[5].StringValue())
 	}
-	if optionsNum == 8 {
-		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[7].StringValue())
+	if optionsNum >= 5 {
+		pollContent = pollContent + fmt.Sprintf("\n5️⃣ %v ", i.ApplicationCommandData().Options[6].StringValue())
 	}
-	if optionsNum == 9 {
-		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[8].StringValue())
+	if optionsNum >= 6 {
+		pollContent = pollContent + fmt.Sprintf("\n6️⃣ %v ", i.ApplicationCommandData().Options[7].StringValue())
+
+	}
+	if optionsNum >= 7 {
+		pollContent = pollContent + fmt.Sprintf("\n7️⃣ %v ", i.ApplicationCommandData().Options[8].StringValue())
+
+	}
+	if optionsNum >= 8 {
+		pollContent = pollContent + fmt.Sprintf("\n8️⃣ %v ", i.ApplicationCommandData().Options[9].StringValue())
+
+	}
+	if optionsNum >= 9 {
+		pollContent = pollContent + fmt.Sprintf("\n9️⃣ %v ", i.ApplicationCommandData().Options[10].StringValue())
+
 	}
 	if optionsNum == 10 {
-		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[9].StringValue())
-	}
-	if optionsNum == 11 {
-		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[10].StringValue())
-	}
-	if optionsNum == 12 {
-		optionsArray = append(optionsArray, i.ApplicationCommandData().Options[11].StringValue())
+		pollContent = pollContent + fmt.Sprintf("\n🔟 %v ", i.ApplicationCommandData().Options[11].StringValue())
+
 	}
 
-	poll = Poll{topic: topic, durationInMinutes: int(duration), options: optionsArray}
+	embed := embed.NewEmbed().
+		SetColor(0x66F442).
+		SetAuthor(i.Member.User.Username, i.Member.User.AvatarURL("24")).
+		SetDescription(pollContent).
+		MessageEmbed
 
-	println(len(poll.options))
+	embeds := []*discordgo.MessageEmbed{embed}
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: embeds,
+		},
+	})
+
+	// time.Sleep(time.Duration(duration))
+	// println(time.Duration(duration).Minutes())
 
 }
