f561f681a574450ee7704b04fada22fa3e846578
archroid <archroid.dev@gmail.com>
2022-01-06 09:56:59 +0330

    ⭐ Clearing the branch

--- main.go
+++ main.go
@@ -1,18 +1,4 @@
 package main
 
-import (
-	"archroid/ElProfessorBot/bot"
-	"archroid/ElProfessorBot/database"
-	"os"
-)
-
 func main() {
-
-	token := os.Getenv("DISCORD_BOT_TOKEN")
-
-	db := database.Start()
-	bot.Start(token, db)
-
-	// <-make(chan struct{})
-	// return
 }
